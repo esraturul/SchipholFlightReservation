4c334e2193bbe2c54f5e53ab6bafceeee99639a8
esraturul <134706376+esraturul@users.noreply.github.com>
2024-05-30 13:59:59 +0300

    Update auth-middleware.ts

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -2,23 +2,20 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const validateReservation = (req: Request, res: Response, next: NextFunction) => {
-  // Burada rezervasyonun doğruluğunu kontrol etmek için gerekli işlemleri gerçekleştirin
-  // Örneğin, gelen verilerin uygunluğunu, varlığını ve geçerliliğini kontrol edebilirsiniz.
+
 
   // Örnek: Gelen istekte reservation adlı bir alanın varlığını kontrol etme
   if (!req.body.reservation) {
     return res.status(400).json({ error: 'Reservation data is missing' });
   }
 
-  // Burada başka kontrolleri ekleyebilirsiniz
 
   // Eğer hata yoksa, bir sonraki middleware'e veya route'a geç
   next();
 };
 
 export const validateRegistration = (req: Request, res: Response, next: NextFunction) => {
-  // Burada kayıt işleminin doğruluğunu kontrol etmek için gerekli işlemleri gerçekleştirin
-  // Örneğin, gelen kullanıcı bilgilerinin uygunluğunu, varlığını ve geçerliliğini kontrol edebilirsiniz.
+
 
   // Örnek: Gelen istekte username, password ve email adlı alanların varlığını kontrol etme
   const { username, password, email } = req.body;
@@ -32,8 +29,6 @@ export const validateRegistration = (req: Request, res: Response, next: NextFunc
   if (!emailRegex.test(email)) {
     return res.status(400).json({ error: 'Invalid email format' });
   }
-
-  // Burada başka kontrolleri ekleyebilirsiniz
 
   // Eğer hata yoksa, bir sonraki middleware'e veya route'a geç
   next();
